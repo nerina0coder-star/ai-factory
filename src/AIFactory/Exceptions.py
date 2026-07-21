fc6e5067c69daeb9d5928cc529b389dc784aa2b3
nerina0coder-star <nerina0coder@gmail.com>
2026-07-21 14:14:46 +0330

    refactor: refactored InDevelopmentException and indev decorator.

--- src/AIFactory/Exceptions.py
+++ src/AIFactory/Exceptions.py
@@ -1,5 +1,3 @@
 
-class InDevelopmentException(BaseException):
-
-    def __init__(self, class_or_function, name) -> None:
-        super().__init__(f"{class_or_function} {name} is in development, please have patience.")
+class InDevelopmentException(Exception):
+    pass
--- src/AIFactory/decorators.py
+++ src/AIFactory/decorators.py
@@ -6,8 +6,5 @@ from AIFactory.Exceptions import InDevelopmentException
 def indev(class_or_function, name) -> Callable:
     def func(self=None, *_, **__):
         _self = self # to stop IDE yelling.
-        raise InDevelopmentException(
-            class_or_function,
-            name
-        )
+        raise InDevelopmentException(f"{class_or_function} {name} is in development, please have patience.")
     return func
